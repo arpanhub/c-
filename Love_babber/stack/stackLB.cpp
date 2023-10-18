29577ff154ab5e89fc350e05ee8ecb7f9bd56b7e
Arpan Gupta <arpan.gupta@lpu.in>
2023-10-19 01:03:11 +0530

    new program in stackLB

--- Love_babber/stack/stackLB.cpp
+++ Love_babber/stack/stackLB.cpp
@@ -1,4 +1,8 @@
 //line->2------------------Stack implementation using array-------------------
+//line->86------------------Design o data structure. which represents two stocks using only one array common for both-------------------
+
+
+
 // #include <iostream>
 // #include <stack>
 // using namespace std;
@@ -83,99 +87,151 @@
 
 
 //Design o data structure. which represents two stocks using only one array common for both
-#include <iostream>
-#include <stack>
-using namespace std;
-class TwoStack{
-    int *arr;
-    int top1;
-    int top2;
-    int size;
-    //inititlaiseing two stack
-    public:
-    TwoStack(int size){
-        this-> size= size;
-        top1= -1;
-        top2= size;
-        arr = new int[size];
-    }
-    void push1(int num){
-        //atleaste ine empty space is avalbale 
-        if(top2-top1 > 1){
-            top1++;
-            arr[top1]=num;
-        }
-        else{
-            cout<<"bhai stack is full:overflow"<<endl;
-        }
-    }
-    void push2(int num){
-        //atleaste ine empty space is avalbale 
-        if(top2-top1 > 1){
-            top2--;
-            arr[top2]=num;
-        }
-        else{
-            cout<<"bhai stack is full:overflow"<<endl;
-        }
-    }
-    int pop1(){
-        if(top1>-1){
-            int ans=arr[top1];
-            top1--;
-            return ans;
-        }
-        else{
-            return -1;
-        }        
-    }
-    int pop2(){
-        if(top2< size){
-            int ans=arr[top1];
-            top2++;
-            return ans;
+// #include <iostream>
+// #include <stack>
+// using namespace std;
+// class TwoStack{
+//     int *arr;
+//     int top1;
+//     int top2;
+//     int size;
+//     //inititlaiseing two stack
+//     public:
+//     TwoStack(int size){
+//         this-> size= size;
+//         top1= -1;
+//         top2= size;
+//         arr = new int[size];
+//     }
+//     void push1(int num){
+//         //atleaste ine empty space is avalbale 
+//         if(top2-top1 > 1){
+//             top1++;
+//             arr[top1]=num;
+//         }
+//         else{
+//             cout<<"bhai stack is full:overflow"<<endl;
+//         }
+//     }
+//     void push2(int num){
+//         //atleaste ine empty space is avalbale 
+//         if(top2-top1 > 1){
+//             top2--;
+//             arr[top2]=num;
+//         }
+//         else{
+//             cout<<"bhai stack is full:overflow"<<endl;
+//         }
+//     }
+//     int pop1(){
+//         if(top1>-1){
+//             int ans=arr[top1];
+//             top1--;
+//             return ans;
+//         }
+//         else{
+//             return -1;
+//         }        
+//     }
+//     int pop2(){
+//         if(top2< size){
+//             int ans=arr[top1];
+//             top2++;
+//             return ans;
                 
-        }
-        else{
-            return -1;
-        }        
-    }
-    int peek1() {
-        if (top1 > -1) {
-            return arr[top1];
-        }
-        else {
-            cout << "Stack 1 is empty" << endl;
-            return -1;
-        }
-    }
+//         }
+//         else{
+//             return -1;
+//         }        
+//     }
+//     int peek1() {
+//         if (top1 > -1) {
+//             return arr[top1];
+//         }
+//         else {
+//             cout << "Stack 1 is empty" << endl;
+//             return -1;
+//         }
+//     }
 
-    int peek2() {
-        if (top2 < size) {
-            return arr[top2];
-        }
-        else {
-            cout << "Stack 2 is empty\n";
-            return -1;
-        }
-    }
-    void display(){
-        int start=top1;
-        int end=top2;
+//     int peek2() {
+//         if (top2 < size) {
+//             return arr[top2];
+//         }
+//         else {
+//             cout << "Stack 2 is empty\n";
+//             return -1;
+//         }
+//     }
+//     void display(){
+//         int start=top1;
+//         int end=top2;
        
-}};
-int main(){
-    TwoStack st(5);
-    st.push1(1);
-    st.push2(5);
-    st.push2(4);
-    st.push2(5);
-    st.push1(3);
-    st.push1(6);
-    cout << "Peek Stack 1: " << st.peek1() << endl;
-    cout << "Peek Stack 2: " << st.peek2() << endl;
-    return 0;
-}
+// }};
+// int main(){
+//     TwoStack st(5);
+//     st.push1(1);
+//     st.push2(5);
+//     st.push2(4);
+//     st.push2(5);
+//     st.push1(3);
+//     st.push1(6);
+//     cout << "Peek Stack 1: " << st.peek1() << endl;
+//     cout << "Peek Stack 2: " << st.peek2() << endl;
+//     return 0;
+// }
 
 
 //Implement stack in arra using linke list
+#include <iostream>
+using  namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+void push(struct Node **top,int value){
+    Node *newNode = new Node();
+    newNode -> data= value;
+    newNode->next= *top;
+    *top=newNode;
+
+}
+void pop(struct Node **top){
+    if(*top == NULL){
+        cout<<"The stack is empty";
+        return;
+    }
+    struct  Node *temp = *top;
+    *top=temp->next;
+    free(top);
+} 
+void display(struct Node *top){
+        struct Node *temp= top;
+        while(temp !=NULL){
+            cout<<temp->data<<" ";
+            temp = temp ->next;
+        }
+        cout<<endl; 
+}
+int main(){
+    struct Node *top = NULL;
+    while(1){
+        int choice,value;
+        cin>>choice;
+        if(choice == 1){
+            cin>> value;
+            push(&top,value);
+        }
+        else if(choice == 2){
+            pop(&top);
+        }
+        else if(choice == 3){
+            display(top);
+        }
+        else{
+            exit(0);    
+        }
+    }
+    return 0;
+}
